07e36edce15ad83883919c6556603d52e226d5a4
Somie1 <somelezesipayile@gmail.com>
2024-01-15 10:28:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.While_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index =0;
+
+            Console.WriteLine("this is the while loop");
+            while (index <= 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            Console.WriteLine("This is the do while loop");
+
+            int index2 = 0;
+            do
+            {
+                Console.WriteLine(index2);
+                index2++;
+            }
+            while (index2 <= 5);
+
+            //freeze console
+            Console.Read();
+            
+
+
+        }
+    }
+}
